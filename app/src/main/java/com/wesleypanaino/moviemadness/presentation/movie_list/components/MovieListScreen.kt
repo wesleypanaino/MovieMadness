a4224f761807a9a2678ff2bd28a6c5ec55b2dd93
whezil <wesleypanaino@gmail.com>
2023-11-01 23:15:39 +1300

    update movielist screen so when scrolled to end of list/grid move items are loaded Also updated grid columns from 2 fixed columns to be adaptive

--- app/src/main/java/com/wesleypanaino/moviemadness/presentation/movie_list/components/MovieListScreen.kt
+++ app/src/main/java/com/wesleypanaino/moviemadness/presentation/movie_list/components/MovieListScreen.kt
@@ -1,21 +1,21 @@
 package com.wesleypanaino.moviemadness.presentation.movie_list.components
 
 import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
@@ -24,22 +24,21 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wesleypanaino.moviemadness.R
 import com.wesleypanaino.moviemadness.domain.model.Movie
 import com.wesleypanaino.moviemadness.presentation.ScreenEvents
@@ -47,7 +46,7 @@ import com.wesleypanaino.moviemadness.presentation.ScreenRoutes
 import com.wesleypanaino.moviemadness.presentation.TopBarWithLogo
 import com.wesleypanaino.moviemadness.presentation.ui.theme.MovieMadnessTheme
 
-private val TAG = "MovieListScreen"
+private const val TAG = "MovieListScreen"
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -56,6 +55,7 @@ fun MovieListScreen(
     onEvent: (ScreenEvents) -> Unit,
 ) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    val context = LocalContext.current
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
 
@@ -69,21 +69,21 @@ fun MovieListScreen(
                     TopBarWithLogo()
                 },
                 actions = {
-                    IconButton(onClick = { onEvent(ScreenEvents.ShowSnackBar("TV Coming soon") )}) {
+                    IconButton(onClick = { Toast.makeText(context, "TV Coming soon",Toast.LENGTH_LONG).show() }) {
                         Icon(
                             painterResource(id = R.drawable.theaters_24px),
                             contentDescription = "Movies",
                             tint = MaterialTheme.colorScheme.onPrimaryContainer,
                         )
                     }
-                    IconButton(onClick = { onEvent(ScreenEvents.ShowSnackBar("Search Coming soon")) }) {
+                    IconButton(onClick = { Toast.makeText(context, "Search Coming soon",Toast.LENGTH_LONG).show() }) {
                         Icon(
                             imageVector = Icons.Filled.Search,
                             contentDescription = "Search",
                             tint = MaterialTheme.colorScheme.onPrimaryContainer,
                         )
                     }
-                    IconButton(onClick = { onEvent(ScreenEvents.ToggleViewMode )}) {
+                    IconButton(onClick = { onEvent(ScreenEvents.ToggleViewMode) }) {
                         Icon(
                             painterResource(
                                 id = when (state.viewMode) {
@@ -100,13 +100,11 @@ fun MovieListScreen(
             )
         },
     ) { paddingValues ->
-        Surface(modifier = Modifier.padding(top = paddingValues.calculateTopPadding())) {
             ScreenContent(
                 paddingValues = paddingValues,
                 state = state,
                 onEvent = onEvent
             )
-        }
     }
 }
 
@@ -116,11 +114,14 @@ fun ScreenContent(
     state: MovieListState,
     onEvent: (ScreenEvents) -> Unit
 ) {
-    Column {
+    val lazyListState = rememberLazyListState()
+    val lazyGridState = rememberLazyGridState()
+    Column(modifier = Modifier.padding( paddingValues)) {
         if (state.isLoading) {
             CircularProgressIndicator(
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-                color = MaterialTheme.colorScheme.onBackground
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .size(16.dp)
             )
         }
         if (state.error.isNotBlank()) {
@@ -136,6 +137,7 @@ fun ScreenContent(
         when (state.viewMode) {
             ViewMode.LIST -> {
                 LazyColumn(
+                    state = lazyListState,
                     modifier = Modifier.fillMaxSize()
                 ) {
                     items(state.movies) { movie ->
@@ -153,35 +155,19 @@ fun ScreenContent(
                         )
                         Divider()
                     }
-                    if (!state.isLoading)
-                        item {
-                            Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                horizontalArrangement = Arrangement.SpaceBetween
-                            ) {
-
-                                IconButton(
-                                    onClick = {
-                                        onEvent(ScreenEvents.PreviousPage)
-                                    },
-                                ) {
-                                    Icon(
-                                        imageVector = Icons.Filled.ArrowBack,
-                                        contentDescription = "Previous Page",
-                                        tint = MaterialTheme.colorScheme.onPrimaryContainer,
-                                    )
-                                }
-
-                                IconButton(onClick = {
-                                    onEvent(ScreenEvents.NextPage)
-                                }
-                                ) {
-                                    Icon(
-                                        imageVector = Icons.Filled.ArrowForward,
-                                        contentDescription = "Next Page",
-                                        tint = MaterialTheme.colorScheme.onPrimaryContainer,
-                                    )
-                                }
+                    item {
+                        if (state.isLoading) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.align(Alignment.CenterHorizontally)
+                            )
+                        }
+                    }
+                }
+                LaunchedEffect(lazyListState) {
+                    snapshotFlow { lazyListState.layoutInfo.visibleItemsInfo.lastOrNull()?.index == lazyListState.layoutInfo.totalItemsCount - 1 }
+                        .collect { endReached ->
+                            if (endReached && !state.isLoading) {
+                                onEvent(ScreenEvents.NextPage)
                             }
                         }
                 }
@@ -189,7 +175,8 @@ fun ScreenContent(
 
             ViewMode.GRID -> {
                 LazyVerticalGrid(
-                    columns = GridCells.Fixed(2), // You can adjust the number of columns here
+                    state = lazyGridState,
+                    columns = GridCells.Adaptive(150.dp),
                     modifier = Modifier.fillMaxSize()
                 ) {
                     items(state.movies) { movie ->
@@ -208,38 +195,17 @@ fun ScreenContent(
                             }
                         )
                     }
-                    if (!state.isLoading)
-                        item {
-                            IconButton(
-                                modifier = Modifier.align(Alignment.Start),
-                                onClick = {
-                                    onEvent(ScreenEvents.PreviousPage)
-                                },
-                            ) {
-                                Icon(
-                                    imageVector = Icons.Filled.ArrowBack,
-                                    contentDescription = "Previous Page",
-                                    tint = MaterialTheme.colorScheme.onPrimaryContainer,
-                                )
-                            }
-                        }
-                    if (!state.isLoading)
-                        item {
-                            IconButton(modifier = Modifier.align(Alignment.End), onClick = {
+                }
+                LaunchedEffect(lazyGridState) {
+                    snapshotFlow { lazyGridState.layoutInfo.visibleItemsInfo.lastOrNull()?.index == lazyGridState.layoutInfo.totalItemsCount - 1 }
+                        .collect { endReached ->
+                            if (endReached && !state.isLoading) {
                                 onEvent(ScreenEvents.NextPage)
-                            }
-                            ) {
-                                Icon(
-                                    imageVector = Icons.Filled.ArrowForward,
-                                    contentDescription = "Next Page",
-                                    tint = MaterialTheme.colorScheme.onPrimaryContainer,
-                                )
                             }
                         }
                 }
             }
         }
-
     }
 }
 
